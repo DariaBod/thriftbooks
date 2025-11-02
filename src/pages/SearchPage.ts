7328b8bcee6d19babb8974fc44df347a1837c52a
dariabodiakova <dashabodiakova0308@gmail.com>
2025-11-01 17:35:34 -0700

    added test, changed ci

--- src/pages/SearchPage.ts
+++ src/pages/SearchPage.ts
@@ -1,42 +1,45 @@
-import {Page, Locator} from '@playwright/test';
+import {Page, Locator, expect} from '@playwright/test';
 
 export class SearchPage {
     readonly page: Page;
     readonly searchInput: Locator;
     readonly searchButton: Locator;
+    readonly searchResults: Locator;
     readonly sortByDrop: Locator;
     readonly moreGenresButton: Locator;
     readonly bookItems: Locator;
-    readonly options: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.searchInput = page.getByRole('textbox', {name: 'Search 19 million titles by'})
         this.searchButton = page.getByRole('button', {name: 'Search Button'})
+        this.searchResults = page.locator("//div[@class = 'AllEditionsItem-tile Recipe-default']");
         this.sortByDrop = page.getByLabel('Sort by:');
-        this.moreGenresButton = page.getByRole('button', { name: 'Expand to show 17 more options' })
+        this.moreGenresButton = page.getByRole('button', {name: 'Expand to show 17 more options'})
         this.bookItems = page.locator("//div/a[@class=\"SearchResultGridItem undefined\"]");
-        this.options = page.locator("//div[@class='Checkbox-label bold']");
     }
 
     async goto() {
         await this.page.goto('/browse/');
     }
 
-     applyOption(title: string) {
-        this.getOption(title).click();
+    async applyOption(title: string) {
+        const option = this.getOption(title);
+        await option.waitFor({ state: 'visible' });
+        await expect(option).not.toHaveClass(/Checkbox-loading/);
+        await option.click();
     }
 
-     getOption(title: string) : Locator {
-      //  return this.options.filter({ hasText: title });
-         return this.options
-             .filter({ hasText: title })
-             .locator('input[type="checkbox"]')
-             .first();
+    getOption(title: string): Locator {
+        return this.page.locator(`//div[text()='${title}']/following-sibling::div`);
     }
 
-    // async applyFilter(filterName: string) {
-    //     await this.filterDropdown.selectOption({ label: filterName });
-    //     await this.page.waitForLoadState('networkidle');
-    // }
+    async expectOptionChecked(title: string, checked = true, timeout = 100000) {
+        const option = this.getOption(title);
+        if (checked) {
+            await expect(option).toBeChecked({ timeout });
+        } else {
+            await expect(option).not.toBeChecked({ timeout });
+        }
+    }
 }
--- src/tests/filters.spec.ts
+++ src/tests/filters.spec.ts
@@ -1,14 +1,38 @@
 import {test, expect} from '../fixtures/SearchFixture';
 
 test.describe('Filters on ThriftBooks', () => {
+
     test('Check books count', async ({searchPage}) => {
         expect(await searchPage.bookItems.count()).toBe(50);
     });
 
-    test('Check selected options', async ({searchPage}) => {
-        expect(await searchPage.bookItems.count()).toBe(50);
+    test('Check multiple filter options', async ({searchPage}) => {
+        const options = ['Movies & TV', 'Books', 'Music', 'Video Games', 'Gifts'];
 
-        await searchPage.applyOption("Movies & TV");
-        await expect(searchPage.getOption("Movies & TV")).toBeChecked();
+        for (const title of options) {
+            await test.step(`Select and verify ${title}`, async () => {
+                await searchPage.applyOption("Movies & TV");
+                await searchPage.expectOptionChecked("Movies & TV");
+                await searchPage.applyOption("Movies & TV");
+            });
+        }
+    });
+
+    test('Check multiple searches results', async ({searchPage}) => {
+        const options = ['harry potter', 'hobbit', 'fahrenheit 451'];
+
+        for (const expectedTitle of options) {
+            await test.step(`Search and verify result for ${expectedTitle}`, async () => {
+                await searchPage.searchInput.fill(expectedTitle);
+                await searchPage.searchButton.click();
+                await searchPage.searchResults.first().waitFor({ state: 'visible', timeout: 10000 });
+                const results = searchPage.searchResults;
+                const count = await results.count() < 10 ? await results.count() : 10;
+                for (let i = 0; i < count; i++) {
+                    const title = await results.nth(i).innerText();
+                    expect(title.toLowerCase()).toContain(expectedTitle);
+                }
+            });
+        }
     });
 });
